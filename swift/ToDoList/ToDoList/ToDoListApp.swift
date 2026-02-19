33dc05d0714d732660bf903a8453c82261185d7f
FRACT <87593133+inoUwU@users.noreply.github.com>
2026-02-19 22:40:52 +0900

    add swift TodoList

---
+++ swift/ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by FRACT on 2026/02/19.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
